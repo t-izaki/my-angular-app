a113ea970c3d2c74b8dac3ec13ceb248805379dc
t-izaki <>
2019-01-01 13:34:26 +0900

    Use service class

---
+++ src/app/hero.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero'
+import { HEROES } from './mock_heroes'
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  // getHeroes(): Hero[] {
+  //   return HEROES;
+  // }
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
+  }
+
+  constructor() { }
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from './../mock_heroes';
+import { HeroService } from './../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,16 +8,21 @@ import { HEROES } from './../mock_heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() { }
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  constructor(private heroService: HeroService) {}
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
